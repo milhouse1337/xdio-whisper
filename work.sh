7623412b5a62a6e8585181baec10ea09cb136913
Pascal Meunier <pascal@trinary.ca>
2024-04-20 15:18:26 -0400

    Fixed hallucination problems

--- work.sh
+++ work.sh
@@ -6,17 +6,20 @@ set -e
 source .env
 
 # whisper=$(which whisper)
-whisper="/Users/pascal/Work/Yodio/whisper.cpp/main"
+whisper="/Users/pascal/Work/Code/whisper.cpp/main"
 
 # model="/whisper/models/ggml-large.bin"
-# model="/Users/pascal/Work/Yodio/whisper.cpp/models/ggml-large.bin"
+# model="/Users/pascal/Work/Code/whisper.cpp/models/ggml-french-v3.bin"
+# model="/Users/pascal/Work/Code/whisper.cpp/models/ggml-large-v2.bin"
+model="/Users/pascal/Work/Code/whisper.cpp/models/ggml-large.bin"
 # model="/Users/pascal/Work/Temp/ggml-french.bin"
-model="/Users/pascal/Work/Temp/ggml-model.bin"
+# model="/Users/pascal/Work/Temp/ggml-model.bin"
+# model="/Users/pascal/Work/Temp/ggml-large.bin"
 
 # -bs 5
-$whisper -l fr -m $model -f "/Users/pascal/Desktop/Projects/Whisper/test-008.wav" -of "test-french" -ovtt -pc -nf -bs 5
-
-exit 0;
+# -nf -bs 5
+# $whisper -l fr -m $model -f "/Users/pascal/niquet1.wav" -of "niquet1" -ovtt -pc -di
+# exit 0;
 
 while true; do
 
@@ -42,8 +45,11 @@ while true; do
         ffmpeg -y -i "xdio-${hash}.mp4" -ar 16000 -ac 1 -c:a pcm_s16le "xdio-${hash}.wav"
 
         # Fine-tuning with: -bs 4 -bo 4
-        # whisper -l fr -m $model -f "xdio-${hash}.wav" -of "xdio-${hash}" -ovtt -pc -bs 4 -bo 4
-        $whisper -l fr -m $model -f "xdio-${hash}.wav" -of "xdio-${hash}" -ovtt -pc -bs 4
+        # time $whisper -l fr -m $model -f "xdio-${hash}.wav" -of "xdio-${hash}" -ovtt -pc -bs 6 -bo 6
+        time $whisper -l fr -m $model -f "xdio-${hash}.wav" -of "xdio-${hash}" -ovtt -pc -bs 5 -et 2.8 -mc 64
+        # $whisper -l fr -m $model -f "xdio-${hash}.wav" -of "xdio-${hash}" -ovtt -pc -et 2.8
+        # time $whisper -l fr -m $model -f "xdio-${hash}.wav" -of "xdio-${hash}" -ovtt -pc
+        # $whisper -l fr -m $model -f "xdio-${hash}.wav" -of "xdio-${hash}" -ovtt -pc -ml 80
 
         # Done.
         task=$(curl -s -H "Accept: application/json" \
@@ -61,6 +67,6 @@ while true; do
 
     fi
 
-    sleep 300
+    sleep 30
 
 done
